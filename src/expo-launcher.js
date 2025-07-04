752e919010242c25dd05f267c398ac96307d8aec
Srinivasa Rao Boyina <2254369+sboyina@users.noreply.github.com>
2025-07-04 12:32:02 +0530

    For platform developers, CLI is crashing when two builds  are happening at the same time.

--- src/expo-launcher.js
+++ src/expo-launcher.js
@@ -345,10 +345,11 @@ function watchForPlatformChanges(callBack) {
             'rn-codegen': getLastModifiedTime(`${codegen}/wavemaker-rn-codegen/dist/new-build`),
             'ui-variables': getLastModifiedTime(`${codegen}/wavemaker-ui-variables/dist/new-build`),
         };
-        const doBuild = lastKnownModifiedTime['rn-runtime'] < currentModifiedTime['rn-runtime']
+        const doBuild = lastKnownModifiedTime['rn-runtime'] > 0 && 
+                (lastKnownModifiedTime['rn-runtime'] < currentModifiedTime['rn-runtime']
                 || lastKnownModifiedTime['rn-codegen'] < currentModifiedTime['rn-codegen']
-                || lastKnownModifiedTime['ui-variables'] < currentModifiedTime['ui-variables'];
-
+                || lastKnownModifiedTime['ui-variables'] < currentModifiedTime['ui-variables']);
+        
         lastKnownModifiedTime = currentModifiedTime;
 
         if (doBuild && callBack) {
